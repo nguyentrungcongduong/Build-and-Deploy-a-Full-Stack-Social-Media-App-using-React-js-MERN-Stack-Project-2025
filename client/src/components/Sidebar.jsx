55fc56188f4887f5fa33a5324bf3b6fb98d71427
Nguyen Van  Manh <nguyentrungcongduong@gmail.com>
2025-08-10 23:17:37 +0700

    tạo sơ lược

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='{w-60 xl:w-72 bg-white border-r border-gray-200 flex flex-col justify-between items-center max-sm:absolute top-0 bottom-0 z-20}'>
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ client/src/pages/Layout.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import Sidebar from "../components/Sidebar";
+import { Outlet } from "react-router-dom";
+import { dummyUserData } from "../assets/assets";
+import Loading from "../components/Loading";
+const Layout = () => {
+  const user = dummyUserData;
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+
+  return user ?  (
+    <div className="w-full flex h-screen">
+      <Sidebar></Sidebar>
+      <div className="flex-1 bg-slate-50">
+        <Outlet></Outlet>
+      </div>
+      {sidebarOpen ? (
+        <X
+          className="absolute top-3 right-3 p-2 z-100 bg-2hite rounded-md shadow w-10 h-10 text-gray-600 sm:hidden"
+          onClick={() => setSidebarOpen(false)}
+        ></X>
+      ) : (
+        <Menu
+          className="absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden"
+          onClick={() => setSidebarOpen(true)}
+        />
+      )}
+    </div>
+  ) : (
+      <Loading></Loading>
+  )
+};
+
+export default Layout;
