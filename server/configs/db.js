a5731a0d8abb16f869e09d691797ddac875baa81
Nguyen Van  Manh <nguyentrungcongduong@gmail.com>
2025-08-28 20:07:28 +0700

    set up vercel

---
+++ server/configs/db.js
@@ -0,0 +1,13 @@
+import mongoose, { mongo }  from "mongoose";
+const connectDB = async ()=>{
+    try{
+        mongoose.connection.on('connected',()=> console.log('Database connected'))
+        await mongoose.connect(`${process.env.MONGODB_URL}/pingup`)
+
+    }
+    catch (error){
+        console.log(error.message)
+    }
+}
+
+export default connectDB;
